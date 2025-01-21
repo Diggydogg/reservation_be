b9e8cb39c122e005975e2c51ac21e4318826089f
Diggydogg <22000471@handong.edu>
2025-01-21 14:53:55 +0900

    first

---
+++ src/main/java/com/reservation/reservation_be/ReservationBeApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.reservation_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reservation/reservation_be/ReservationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.reservation_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
